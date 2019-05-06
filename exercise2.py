dbbda1ef5e94888cc2eb61df6656031e44ebfa14
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-06 13:46:01 -0400

    completed exercise 2

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+#calculating tip for $55 meal
+print(55*0.2)
+
+#total amount (adding string with integer)
+print("Total = " + str(55*1.2))
+
+#result of 45628 * 7839
+print(str(456728*7839))
+
+#What is the value?
+print((10 < 20 and 30 < 20) or not(10 ==11))
