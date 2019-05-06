4203262a3a4f171a106abcc3abae169a49e58dc5
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-06 14:20:11 -0400

    organized exercise 3

--- exercise3.py
+++ exercise3.py
@@ -1,30 +1,30 @@
-# #Part 1
-# # print("What is your name?")
-# # user_name = input()
-# # print("Hello, {}".format(user_name))
+#Part 1
+print("What is your name?")
+user_name = input()
+print("Hello, {}".format(user_name))
 
 
-# #Part 2
-# # secret_password = "please"
+#Part 2
+secret_password = "please"
 
-# # print("What's the password?")
-# # password_attempt = input()
+print("What's the password?")
+password_attempt = input()
 
-# # correct_or_not = (password_attempt == secret_password)
+correct_or_not = (password_attempt == secret_password)
 
-# # print("That's {}!".format(correct_or_not))
-
-
-# #Part 3
-# print("How many cookies have been eaten?")
-# eaten = input()
-
-# #convert user input to integer and subtract it from 12
-# remaining_cookies = 12 - int(eaten)
-
-# print("There are {} cookies left.".format(remaining_cookies))
+print("That's {}!".format(correct_or_not))
 
 
+#Part 3
+print("How many cookies have been eaten?")
+eaten = input()
+
+remaining_cookies = 12 - int(eaten)
+
+print("There are {} cookies left.".format(remaining_cookies))
+
+
+#Part 4
 print("How old will you be this year?")
 age = input()
 birth_year = 2019 - int(age)
